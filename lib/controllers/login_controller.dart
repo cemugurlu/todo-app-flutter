6f0fae23b5ffb828d5a83a7eda77836c05815015
Cem Uğurlu <61113120+cemugurlu@users.noreply.github.com>
2024-05-13 22:46:11 +0300

    login controller added

---
+++ lib/controllers/login_controller.dart
@@ -0,0 +1,24 @@
+import 'package:get/get.dart';
+
+class LoginController extends GetxController {
+  final email = ''.obs;
+  final password = ''.obs;
+  final isObscure = true.obs;
+
+  RxBool isButtonEnabled = false.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    debounce(email, (_) => updateButtonState(), time: const Duration(milliseconds: 10));
+    debounce(password, (_) => updateButtonState(), time: const Duration(milliseconds: 10));
+  }
+
+  void updateButtonState() {
+    isButtonEnabled.value = email.value.length >= 3 && password.value.length >= 6;
+  }
+
+  void togglePasswordVisibility() {
+    isObscure.value = !isObscure.value; // Toggle the visibility state
+  }
+}
